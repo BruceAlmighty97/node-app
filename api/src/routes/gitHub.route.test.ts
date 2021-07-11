8fa541aaae65d65487126dfea5750d24ae3c89f1
Geoff <geoff.dev.74@gmail.com>
2021-07-11 17:15:39 -0500

    remove useless test add test for invalid body with no url

--- api/src/routes/gitHub.route.test.ts
+++ api/src/routes/gitHub.route.test.ts
@@ -2,13 +2,15 @@ import request from 'supertest'
 import { app } from '../app'
 
 describe("testing github routes", () => {
-    it('should true', () => {
-        expect(true).toBe(true);
-    });
-
     it('should return ok', async () => {
         const result = await request(app).get('/').send();
         expect(result.statusCode).toBe(200);
         expect(result.text).toEqual('node app running');
+    });
+
+    it('should return 400 with bad request', async () => {
+        const result = await request(app).post('/get-open-prs').send({});
+        expect(result.statusCode).toBe(400);
+        expect(result.text).toEqual('invalid request');
     });
 })
