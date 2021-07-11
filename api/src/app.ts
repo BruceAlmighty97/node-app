de3e187de0b710c41537aa3817ab0c0694f5857e
Geoff <geoff.dev.74@gmail.com>
2021-07-11 13:24:54 -0500

    refactor message

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,8 +1,8 @@
 import express from 'express';
 import cors from 'cors';
+import { runningMessage } from './index';
 
 export const app: express.Application = express();
-const runningMessage = "Node application is running";
 
 app.use(express.json());
 app.use(cors());
