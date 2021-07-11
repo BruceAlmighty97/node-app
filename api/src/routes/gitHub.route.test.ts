f8515b8d647195cc892bc7b9a4a64173c8d7ba33
Geoff <geoff.dev.74@gmail.com>
2021-07-11 16:53:22 -0500

    fix import issue add base unit test for base path

--- api/src/routes/gitHub.route.test.ts
+++ api/src/routes/gitHub.route.test.ts
@@ -1,5 +1,14 @@
+import request from 'supertest'
+import { app } from '../app'
+
 describe("testing github routes", () => {
     it('should true', () => {
         expect(true).toBe(true);
-    })
+    });
+
+    it('should return ok', async () => {
+        const result = await request(app).get('/').send();
+        expect(result.statusCode).toBe(200);
+        expect(result.text).toEqual('node app running');
+    });
 })
