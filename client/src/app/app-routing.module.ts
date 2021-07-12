84b9f01fe9d7381ce5235004116c97ea26067c2a
Geoff <geoff.dev.74@gmail.com>
2021-07-11 21:57:01 -0500

    finish render component add default routing to github

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { GithubPageComponent } from './components/github-page/github-page.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/github', pathMatch: 'full' },
   { path: 'github', component: GithubPageComponent }
 ];
 
